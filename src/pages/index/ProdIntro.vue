72ee255173c0b7f779b8eff88c795af9c80308f5
seeacloud <seeacloud@gmail.com>
2017-09-11 18:03:42 +0800

    milestone

--- src/pages/index/ProdIntro.vue
+++ src/pages/index/ProdIntro.vue
@@ -1,8 +1,29 @@
 <template>
+  <div style="background:#ffffff;">
     <div class="con">
-      <h1>山木通 让管理更轻松</h1>
-      <h5>山木通运用国际先进的互联网、云计算、大数据等技术，结合高品质的客户服务，已为多家企业提供了企业运营与管理完整的互联网解决方案。</h5>
+      <div class="sec-head">
+        <h1 class="sec-title">山木通 让管理更轻松</h1>
+        <h5 class="sub-title">山木通运用国际先进的互联网、云计算、大数据等技术，结合高品质的客户服务，已为多家企业提供了企业运营与管理完整的互联网解决方案。</h5>
+      </div>
+      <Row>
+        <Col span="6">
+        asa
+        </Col>
+        <Col span="6">
+        </Col>
+        <Col span="6">
+        </Col>
+        <Col span="6">
+        xxx
+        </Col>
+      </Row>
+
+
     </div>
+
+    <div style="background:#dddddd;">xxx</div>
+  </div>
+
 </template>
 
 <script>
@@ -17,5 +38,19 @@
 <style lang='scss' scoped>
   @import "../../../static/common";
 
+  .con {
+    background: #ffffff;
+    padding: 40px 0;
+  }
+
+  .sec-head {
+    width: 60%;
+    margin-left: 20%;
+  }
+
+  .sec-title {
+    text-align: center;
+    color: $smtred;
+  }
 
 </style>

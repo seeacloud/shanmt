83e5db844e9bc825f5639a35930fd47d9d5bc403
seeacloud <seeacloud@gmail.com>
2017-09-16 23:08:08 +0800

    milestone

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,35 +1,17 @@
 <template>
-  <div style="background:#ffffff;border-bottom:1px solid #eeeeee;">
-    <div class="eyebrow">
-      <div class="con">
-        <a href="http://www.moyasz.com" target="_blank" style="margin-right:20px;">莫亚科技旗下产品</a>
-        <span class="phone"><i class="iconfont" style="line-height:30px;vertical-align: bottom;margin-right:5px;">&#xe682;</i>咨询电话: 0755-8657 5595</span>
-      </div>
-    </div>
-    <div class="menu">
-      <div class="con">
-        <div class="menu-items">
-
-          <div v-for="(item,index) in items" :key="index" class="item-wrap" :id="item.name"
-               @mouseenter="showSub($event)" @mouseleave="moveout" @click="moveout">
-            <a :href="item.link" :class="{active:activemenu==item.name}"
-            >{{item.title}}</a>
-            <div class="sub-menu" v-show="currentItem=='products'">
-              <a v-for="(sub,index) in item.sub" :key="index" :href="sub.link">{{sub.title}}</a>
-            </div>
-          </div>
-
+  <div style="background:#ffffff;">
+    <div class="con">
+      <div class="nav">
+        <div class="logo">
+          <img src="../../static/img/logo.jpg" alt=""/>
         </div>
-        <a href="index.html" target="_self" id="a-logo">
-          <img :src="logoUrl">
-        </a>
-        <div class="log-items">
-          <a href="http://shanmt.com/public/trySmt" class="a-btn">免费试用</a>
-          <a href="login.html#/login">登录</a>
-          <a href="login.html#/signup">注册</a>
+        <div class="divsearch"><input type="text"> <i class="iconfont">&#xe600;</i></div>
+        <div class="divnav">
+          <a v-for="(item, index) in items" :key="index" class="navitem" :href="item.link">
+            {{item.title}}
+          </a>
         </div>
       </div>
-
     </div>
   </div>
 </template>
@@ -41,166 +23,86 @@
   Vue.use(Router)
 
   export default {
-    name: 'header',
-    props: ['activemenu'],
+    name: '',
     data () {
       return {
-        currentItem: '',
-        logoUrl: './static/img/smtlogo.jpg',
         items: [
-          {name: 'index', title: '首页', link: 'index.html', sub: []},
-          {
-            name: 'products',
-            title: '产品',
-            link: 'products.html#/smtm',
-            sub: [
-              {title: '云订货平台', link: 'products.html#/order'},
-              {title: '云进销存', link: 'products.html#/jxc'},
-              {title: '建材家具厂家版', link: 'products.html#/smtm'},
-              {title: '建材家具门店版', link: 'products.html#/smtagent'}
-            ]
-          },
-          {name: 'buy', title: '购买', link: 'buy.html', sub: []},
-//          {name: 'join', title: '合作', link: 'join.html', sub: []},
-          {name: 'help', title: '帮助', link: 'help.html', sub: []},
-          {name: 'about', title: '关于山木通', link: 'about.html#/intro', sub: []}
+          {title: 'HOME', link: 'about.html'},
+          {title: 'ABOUT US', link: 'about.html'},
+          {title: 'PRODUCTS', link: 'about.html'},
+          {title: 'NEWS', link: 'about.html'},
+          {title: 'CONTACT US', link: 'about.html'}
         ]
-      }
-    },
-    methods: {
-      showSub: function (e) {
-        this.currentItem = e.target.id
-        console.log(this.currentItem)
-      },
-      moveout: function () {
-        console.log('move out')
-        this.currentItem = ''
       }
     }
   }
 </script>
 
 <style lang='scss' scoped>
-  @import "../../static/common";
+  @import "../../static/normal";
 
-  .eyebrow {
-    text-align: right;
-    border-bottom: 1px solid #eeeeee;
-    height: 30px;
-    font-size: 12px;
-    min-width: 1180px;
-    .con {
-      height: 30px;
-    }
-
-  }
-
-  .active {
-    color: $smtred;
-  }
-
-  .phone {
-    color: $smtred;
-    font-size: inherit;
-    line-height: 30px;
-  }
-
-  .menu {
-    height: 60px;
-    width: 100%;
-    min-width: 1180px;
-    display: block;
-    .con {
-      position: relative;
-    }
-    i-button {
-      display: inline-block;
-      vertical-align: baseline;
-    }
-  }
-
-  .menu-items {
-    text-align: center;
-    height: 60px;
-    line-height: 60px;
-    font-size: 16px;
-    a {
-      display: inline-block;
-      margin: 0 15px;
-      font-size: inherit;
-      line-height: 30px;
-    }
-  }
-
-  .item-wrap {
-    display: inline-block;
-    position: relative;
-  }
-
-  @media screen and(min-width: 1650px) {
-    .menu-items {
-      a {
-        font-size: 18px;
-      }
-    }
-
-  }
-
-  .sub-menu {
-    width: 150px;
-    background: #ffffff;
-    position: absolute;
-    z-index: 999;
-    top: 60px;
-    left: -5px;
+  .con {
+    height: 200px;
     font-size: 0;
-    line-height: 0;
-    a {
-      display: block;
-      color: #666666;
-      font-size: 12px;
-      margin: 0;
-      padding: 0;
-      line-height: 40px;
-      text-align: left;
-      padding-left: 15px;
-      border-bottom: 1px solid #eeeeee;
-    }
-    a:hover {
-      color: $smtred;
-    }
   }
 
-  #a-logo {
+  .logo {
     position: absolute;
+    top: 0;
     left: 0;
-    top: 10px;
+    width: 200px;
+    height: 100%;
   }
 
-  .log-items {
+  .nav {
+    position: relative;
+    padding-left: 200px;
+    height: 100%;
+    text-align: right;
+  }
+
+  .navitem {
+    display: inline-block;
+    padding: 0 0 0 80px;
+    font-size: 18px;
+    height: 100%;
+    line-height: 70px;
+    color: #333333;
+  }
+  .navitem:hover
+  {
+    color: $blue;
+  }
+
+  .divsearch {
+    position: relative;
+    height: 40px;
+    input[type="text"] {
+      height: 45px;
+      font-size: 16px;
+      width: 250px;
+      line-height: 38px;
+      outline: none;
+      padding: 0 30px 0 10px;
+      margin-top: 40px;
+      border: 2px solid #dddddd;
+      border-radius: 3px;
+    }
+    .iconfont {
+      display: inline-block;
+      position: absolute;
+      right: 10px;
+      top: 40px;
+      line-height: 45px;
+      font-weight: bold;
+      font-size: 24px;
+      color: #aaaaaa;
+    }
+  }
+
+  .divnav {
     position: absolute;
     right: 0;
-    top: 0;
-    height: 90px;
-    a {
-      font-size: 14px;
-      margin: 0 5px;
-      height: 30px;
-      line-height: 30px;
-      margin-top: 15px;
-    }
-
-    .a-btn {
-      display: inline-block;
-      background: $blue;
-      padding: 0 15px;
-      font-size: 12px;
-      border-radius: 15px;
-      color: #ffffff;
-    }
-    .a-btn:hover {
-      background: $lightblue;
-    }
+    bottom: 21px;
   }
-
 </style>
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,26 +1,10 @@
-/**
- * Created by alex on 9/5/17.
- */
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import HeaderItem from '../../components/Header.vue'
-import FooterItem from '../../components/Footer.vue'
-import '../../../static/iconfont/iconfont.css'
+import FooterItem from '../../components/Footer'
 import iView from 'iview'
 import 'iview/dist/styles/iview.css'
-import Service from '../../components/Service.vue'
-import Crsl from './Crsl.vue'
-import '../../../static/normal.css'
-import SecLeft from '../../components/SecLeft.vue'
-import SecRight from '../../components/SecRight.vue'
-import Banner from '../../components/Banner.vue'
-import ProdIntro from './ProdIntro.vue'
-import Improve from './Improve.vue'
-import Brands from '../../components/Brands.vue'
-import Router from 'vue-router'
+import '../../../static/iconfont/iconfont.css'
 
-Vue.use(Router)
 Vue.use(iView)
 
 // Vue.config.productionTip = false
@@ -29,36 +13,7 @@ Vue.use(iView)
 new Vue({
   el: '#app',
   data: {
-    switzer: './static/img/switzerland.jpg',
-    secinfo: {
-      title: '解决传统加工企业8大难题',
-      subtitle: '',
-      captitle: '拥抱变革',
-      caption: '在互联网和云计算的浪潮下，传统企业要想突围，必须主动迎接变化，进行自我革新',
-      btn: '',
-      imgurl: './static/img/mac.png',
-      push: 0
-    },
-    secinfo2: {
-      title: '',
-      subtitle: '',
-      captitle: '拥抱变革',
-      caption: '在互联网和云计算的浪潮下，传统企业要想突围，必须主动迎接变化，进行自我革新',
-      btn: '了解详情',
-      imgurl: './static/img/trend.jpg',
-      push: 12
-    },
-    secinfo3: {
-      title: '',
-      subtitle: '',
-      captitle: '拥抱变革',
-      caption: '在互联网和云计算的浪潮下，传统企业要想突围，必须主动迎接变化，进行自我革新',
-      btn: '了解详情',
-      imgurl: './static/img/air-iphone.png',
-      push: 12
-    },
-    bannerinfo: {title: 'banner', imgurl: './static/img/banner-cloud.jpg', txtalign: ''}
-
+    msg: 'geo'
   },
-  components: {HeaderItem, FooterItem, Service, Crsl, SecLeft, SecRight, Banner, ProdIntro, Improve, Brands}
+  components: {HeaderItem, FooterItem}
 })
